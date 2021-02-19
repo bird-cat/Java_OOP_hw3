90ac50ec0a5dbc0058f56a30650efb5ecd521384
tupolev4 <tupolev4@gmail.com>
2021-02-19 20:35:48 +0800

    Fix: PlayGame
    * Allow the user to choose the variant of old maid game and the number of player in the main function

--- PlayGame.java
+++ PlayGame.java
@@ -2,11 +2,32 @@ import java.util.*;
 import ntu.r09922114.util.*;
 import ntu.r09922114.binaryTree.*;
 import ntu.r09922114.container.*;
+import ntu.r09922114.gambling.Strict_pair_old_maid;
+import ntu.r09922114.gambling.NoJoker_old_maid;
 import ntu.r09922114.gambling.Old_maid;
 
 public class PlayGame {
     public static void main(String [] argv) {
-        Old_maid game = new Old_maid();
+        Old_maid game;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose the type of old maid((a) Traditional old maid, (b) No Joker, (c) Strict pair definition): ");
+        char choice = sc.nextLine().charAt(0);
+        System.out.print("Determine the number of player(from 2 to 52): ");
+        int playerCount = sc.nextInt();
+        switch (choice) {
+            case 'a':
+                game = new Old_maid(playerCount);
+                break;
+            case 'b':
+                game = new NoJoker_old_maid(playerCount);
+                break;
+            case 'c':
+                game = new Strict_pair_old_maid(playerCount);
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                return;
+        }
         game.startGame();
     }
 }
